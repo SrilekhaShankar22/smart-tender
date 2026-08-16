d635fb593e90d0f06ca5219a55040fb34f0d59c6
SrilekhaShankar22 <118003240+SrilekhaShankar22@users.noreply.github.com>
2026-08-16 11:49:00 +0530

    Correcting Build failure

--- tender-fetch-service/src/test/java/com/smarttender/fetch/TenderFetchServiceTest.java
+++ tender-fetch-service/src/test/java/com/smarttender/fetch/TenderFetchServiceTest.java
@@ -12,14 +12,18 @@ import com.smarttender.fetch.service.impl.TenderFetchService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.*;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 import java.time.LocalDateTime;
 import java.util.List;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 class TenderFetchServiceTest {
 
     @Mock GemPortalScraper scraper;
